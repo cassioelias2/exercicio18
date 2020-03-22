6fd036afb68cb3a6e188fd512d7ab63296f1297d
Cassio <cassio_qwe@outlook.com>
2020-03-22 14:40:52 -0300

    Gerando gols para os times dentro da lista

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var util = require('../public/javascripts/gol');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  var times = [
+    {"time1": {"id": 1, "nome": "Corinthians", "gols": 0}, "time2": {"id": 2, "nome": "Vasco", "gols": 0}},
+    {"time1": {"id": 1, "nome": "Grêmio", "gols": 0}, "time2": {"id": 2, "nome": "Inter", "gols": 0}}
+  ];
+
+  setInterval(() => {
+    times.forEach(time => {
+      new Promise((res, rej) => {
+        let gol = util.gol();
+        if (time.time1.id == gol) {
+          time.time1.gols++;
+        } else if(time.time2.id == gol) {
+          time.time2.gols++;
+        }
+      });
+      console.log(time.time1.nome + ' ' + time.time1.gols + ' X ' + time.time2.nome + ' ' + time.time2.gols);
+    });
+  }, 4500);
+
+  res.send('ok');
 });
 
 module.exports = router;
